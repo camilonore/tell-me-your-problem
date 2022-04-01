b1aeb85a47c30feda2577d19c6092c7b5738ca65
Camilo292 <caminore22@gmail.com>
2022-04-01 12:57:38 -0500

    Comment Schema updated

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,11 @@
 import { Schema, model, models } from 'mongoose'
 
 const commentSchema = new Schema({
+  postReference: {
+    type: Schema.ObjectId,
+    ref: 'Comment',
+    required: true
+  },
   author: {
     type: Schema.ObjectId,
     ref: 'User',
