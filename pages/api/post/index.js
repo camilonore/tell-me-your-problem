922875b859b82b8584cdf9dffe13e436e6ed616d
Camilo292 <caminore22@gmail.com>
2022-04-01 11:49:40 -0500

    Implementing methods to post endpoint

--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -1,8 +1,41 @@
+import { connectToDatabase } from '../../../Utils/connectDb'
+import { PostModel } from '../../../models/post'
+import { responseSuccess, responseError } from '../../../Utils/responses'
+import { isEmpty } from '../../../Utils/isEmpty'
+
 export default function handler (req, res) {
+  // TODO: Check if the user exists
+  // TODO: Cache the data
   if (req.method === 'POST') {
-    res.send('Post comments')
+    if (isEmpty({ ...req.body })) {
+      responseError(res, 'Check your data', 400, 'Incomplete data')
+    } else {
+      (async () => {
+        await connectToDatabase()
+        const newPost = new PostModel({ ...req.body })
+        await newPost.save()
+        PostModel.find({})
+          .populate('author')
+          .exec((err, populatedData) => {
+            if (err) {
+              responseError(res, 'Internal error', 400, err)
+            }
+            responseSuccess(res, populatedData, 200)
+          })
+      })()
+    }
   }
   if (req.method === 'GET') {
-    res.send('Get comments')
+    (async () => {
+      await connectToDatabase()
+      PostModel.find({})
+        .populate('author')
+        .exec((err, populatedData) => {
+          if (err) {
+            responseError(res, 'Internal error', 400, err)
+          }
+          responseSuccess(res, populatedData, 200)
+        })
+    })()
   }
 }
