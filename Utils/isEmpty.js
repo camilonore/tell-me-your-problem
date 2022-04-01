bd59b53a960e1aeac0be7afcc73f8daf83e96f63
Camilo292 <caminore22@gmail.com>
2022-04-01 11:48:56 -0500

    Improving algorithm

--- Utils/isEmpty.js
+++ Utils/isEmpty.js
@@ -1,7 +1,8 @@
-function isEmpty (data) {
-  if (!data) {
-    return true
-  }
-  return false
+function isEmpty (body) {
+  if (Object.keys(body) <= 0) return true
+  const emptyKeys = Object.keys(body).filter((key) => {
+    return body[key].length <= 0
+  })
+  return emptyKeys.length > 0
 }
 export { isEmpty }
