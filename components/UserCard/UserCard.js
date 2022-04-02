b2475c4c2eef125470d869db9c8c710f904a884e
Camilo292 <caminore22@gmail.com>
2022-04-01 22:39:51 -0500

    img to Image

--- components/UserCard/UserCard.js
+++ components/UserCard/UserCard.js
@@ -1,10 +1,11 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from './UserCard.module.css'
 
 const UserCard = ({ img, name }) => {
   return (
     <div className={styles.userCard}>
-      <img src={img} alt={name} />
+      <Image src={img} alt={name} />
       <Link href={'/p/' + name}>
         <a>{name}</a>
       </Link>
