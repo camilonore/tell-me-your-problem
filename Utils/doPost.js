dc24a3aa3479ba4a118750a90d822c310eb7ab3e
Camilo292 <caminore22@gmail.com>
2022-04-04 10:05:36 -0500

    async function

--- Utils/doPost.js
+++ Utils/doPost.js
@@ -1,10 +1,11 @@
-function doPost (url, body) {
-  fetch(url, {
+async function doPost (url, body) {
+  const response = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(body)
   })
+  return await response.json()
 }
 export { doPost }
