52ee71ee588b4fbda8932289729479ba1aac2997
Camilo292 <caminore22@gmail.com>
2022-04-06 09:36:22 -0500

    No mocks in render

--- components/Aside/Aside.js
+++ components/Aside/Aside.js
@@ -1,16 +1,15 @@
 import UserCard from '../UserCard/UserCard'
 import styles from './Aside.module.css'
-import { usersMock } from '../../mocks/usersMock'
 
 const Aside = () => {
   return (
     <aside className={styles.aside}>
       <p>Popular Profiles</p>
-      {
+      {/* {
         usersMock.map(user => {
           return <UserCard key={user.id} img={user.img} name={user.name}/>
         })
-      }
+      } */}
     </aside>
   )
 }
