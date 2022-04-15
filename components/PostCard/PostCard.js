4adb31cfb23f29cd8b0248c96738c4170e65b6b6
Camilo292 <caminore22@gmail.com>
2022-04-15 11:39:05 -0500

    Likes hook

--- components/PostCard/PostCard.js
+++ components/PostCard/PostCard.js
@@ -3,45 +3,10 @@ import { PostHeader } from '../PostHeader/PostHeader'
 import { Message } from '../../icons/Message'
 import { Heart } from '../../icons/Heart'
 import { Button } from '../Button/Button'
-import { doPost } from '../../services/doPost'
-import { doDelete } from '../../services/doDelete'
-import { useState } from 'react'
-import { useSession } from 'next-auth/react'
+import { useLike } from '../../hooks/useLike'
 
-// TODO: Refactor!
-// TODO: Add asynchronysm
 const PostCard = ({ post }) => {
-  const [disabled, setDisabled] = useState(false)
-  const { data: session } = useSession()
-  const isLiked = post.likes.indexOf(parseInt(session.user.userId))
-  const [like, setLike] = useState(isLiked)
-  const [numOfLikes, setNumOfLikes] = useState(post.likes.length)
-
-  const handleClick = (evt) => {
-    evt.preventDefault()
-    if (like !== -1) {
-      setDisabled(true)
-      doDelete('/api/post/like', {
-        id: post._id,
-        author: session.user.userId
-      }).then(() => {
-        setLike(-1)
-        setNumOfLikes(numOfLikes - 1)
-        setDisabled(false)
-      })
-      return false
-    } else {
-      setDisabled(true)
-      doPost('/api/post/like', {
-        id: post._id,
-        author: session.user.userId
-      }).then(() => {
-        setDisabled(false)
-        setLike(true)
-        setNumOfLikes(numOfLikes + 1)
-      })
-    }
-  }
+  const { isLiked, numOfLikes, handleClick } = useLike(post.likes, post._id)
   return (
       <section className={styles.card}>
         <PostHeader {...post}/>
@@ -53,8 +18,8 @@ const PostCard = ({ post }) => {
             {post.comments.length}
           </span>
           <span>
-            <Button onClick={handleClick} disabled={disabled}>
-              <Heart width={18} height={18} fill={like !== -1 ? 'red' : 'none'}/>
+            <Button onClick={handleClick}>
+              <Heart width={18} height={18} fill={isLiked ? 'red' : 'none'}/>
               {numOfLikes}
             </Button>
           </span>
---
+++ hooks/useLike.js
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import { doDelete } from '../services/doDelete'
+import { doPost } from '../services/doPost'
+
+function useLike (likes, postId) {
+  const { data: session, status } = useSession()
+  const [isLiked, setIsLiked] = useState(false)
+  const [numOfLikes, setNumOfLikes] = useState(likes.length)
+  const [authenticated, setAuthenticated] = useState(false)
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      setAuthenticated(true)
+      const index = likes.indexOf(parseInt(session.user.userId))
+      if (index !== -1) {
+        setIsLiked(true)
+      }
+    }
+  }, [likes, session?.user?.userId, status])
+
+  const handleClick = (evt) => {
+    evt.preventDefault()
+    if (!authenticated) {
+      // TODO: useState(errors)
+      alert('Please SignIn to like the post')
+      return
+    }
+    setIsLiked(!isLiked)
+    const url = '/api/post/like'
+    const body = {
+      id: postId,
+      author: session.user.userId
+    }
+    if (isLiked) {
+      setNumOfLikes(numOfLikes - 1)
+      doDelete(url, body)
+        .catch(() => {
+          setIsLiked(!isLiked)
+          setNumOfLikes(numOfLikes + 1)
+        })
+    } else {
+      setNumOfLikes(numOfLikes + 1)
+      doPost(url, body)
+        .catch(() => {
+          setIsLiked(!isLiked)
+          setNumOfLikes(numOfLikes - 1)
+        })
+    }
+  }
+
+  return { isLiked, numOfLikes, handleClick }
+}
+
+export { useLike }
