f1ea87a3be07c75a9dbf912aa4675a3ad890ef5a
Camilo292 <caminore22@gmail.com>
2022-04-17 19:54:43 -0500

    Loader

--- components/Aside/Aside.js
+++ components/Aside/Aside.js
@@ -1,34 +1,20 @@
 import UserCard from '../UserCard/UserCard'
 import styles from './Aside.module.css'
-import { doGet } from '../../services/doGet'
-import { useState, useEffect } from 'react'
-import { Loading } from '../Loading/Loading'
 
-const Aside = () => {
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(() => {
-    setLoading(true)
-    doGet('/api/user')
-      .then((users) => {
-        setUsers(users)
-        setLoading(false)
-      })
-      .catch(() => {
-        setLoading(false)
-      })
-  }, [])
-
+const Aside = ({ users }) => {
   return (
     <aside className={styles.aside}>
       <p>Popular Profiles</p>
-      {loading
-        ? <Loading />
-        : users.map((user) => {
-          return <UserCard key={user._id} img={user.img} name={user.username} id={user._id} />
-        }
-        )}
+      {users.map((user) => {
+        return (
+          <UserCard
+            key={user._id}
+            img={user.img}
+            name={user.username}
+            id={user._id}
+          />
+        )
+      })}
     </aside>
   )
 }
