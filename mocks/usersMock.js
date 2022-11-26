c207500f9b80c1a44de9e0c84fbca3a47b7d36c1
Camilo Nore <caminore22@gmail.com>
2022-11-26 12:34:38 -0500

    Updated

---
+++ mocks/usersMock.js
@@ -0,0 +1,28 @@
+const usersMock = [
+  {
+    id: '@CamiloNore',
+    img: 'https://picsum.photos/200',
+    name: '@CamiloNore'
+  },
+  {
+    id: '@Midudev',
+    img: 'https://picsum.photos/200',
+    name: '@Midudev'
+  },
+  {
+    id: '@WillSmith',
+    img: 'https://picsum.photos/200',
+    name: '@WillSmith'
+  },
+  {
+    id: '@ElonMusk',
+    img: 'https://picsum.photos/200',
+    name: '@ElonMusk'
+  },
+  {
+    id: '@AlbertEinstein',
+    img: 'https://picsum.photos/200',
+    name: '@AlbertEintein'
+  }
+]
+export { usersMock }
